e7931c5e393e2171681c560762d11ef1072eb228
Garrett Hensley <hensley.garrett@gmail.com>
2013-05-29 16:59:47 -0400

    test

--- PoleSearch/src/main/java/com/glps/polesearch/MainActivity.java
+++ PoleSearch/src/main/java/com/glps/polesearch/MainActivity.java
@@ -3,11 +3,10 @@ package com.glps.polesearch;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import android.app.ActionBar;
-import android.app.AlertDialog;
-import android.app.FragmentTransaction;
-import android.content.DialogInterface;
-import android.content.Intent;
+
+
+import android.app.*;
+import android.content.*;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
@@ -20,12 +19,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -34,6 +31,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 
@@ -41,24 +40,12 @@ public class MainActivity extends FragmentActivity implements
         ActionBar.TabListener,
         LocationListener,
         GooglePlayServicesClient.OnConnectionFailedListener,
-        GooglePlayServicesClient.ConnectionCallbacks
-        {
+        GooglePlayServicesClient.ConnectionCallbacks {
 
-    // Global constants (first 5 specifically for location services)
 
-    // Milliseconds per second
-    private static final int MILLISECONDS_PER_SECOND = 1000;
-    // Update frequency in seconds
-    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
-    // Update frequency in milliseconds
-    private static final long UPDATE_INTERVAL = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
-    // The fastest update frequency, in seconds
-    private static final int FASTEST_INTERVAL_IN_SECONDS = 1;
-    // A fast frequency ceiling in milliseconds
-    private static final long FASTEST_INTERVAL = MILLISECONDS_PER_SECOND * FASTEST_INTERVAL_IN_SECONDS;
-
-    // Define an object that holds accuracy and frequency parameters
-    LocationRequest mLocationRequest;
+    private LocationRequest mLocationRequest;
+    private LocationClient mLocationClient;
+    boolean mUpdatesRequested = false;
 
     private static Handler hm;
     private static int GRID_SEARCH = 1;
@@ -70,6 +57,11 @@ public class MainActivity extends FragmentActivity implements
     final PoleDatabase datasource = new PoleDatabase(this);
     private static final int TEXT_VIEW_CNT = 5;
     private TextView[] textViewArray = new TextView[TEXT_VIEW_CNT];
+    // Handle to SharedPreferences for this app
+    SharedPreferences mPrefs;
+
+    // Handle to a SharedPreferences editor
+    SharedPreferences.Editor mEditor;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -140,7 +132,23 @@ public class MainActivity extends FragmentActivity implements
             }
         };
 
-        //datasource.open();
+        datasource.open();
+        mLocationRequest = LocationRequest.create();
+        /*
+         * Set the update interval
+         */
+        mLocationRequest.setInterval(LocationUtils.UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Use high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // Set the interval ceiling to one minute
+        mLocationRequest.setFastestInterval(LocationUtils.FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+
+        // Note that location updates are off until the user turns them on
+        mUpdatesRequested = false;
+        mLocationClient = new LocationClient(this,this,this);
+
 
         latitudeField = (TextView) findViewById(R.id.TextView02);
         longitudeField = (TextView) findViewById(R.id.TextView04);
@@ -150,12 +158,81 @@ public class MainActivity extends FragmentActivity implements
         textViewArray[2] = (TextView) findViewById(R.id.TextView07);
         textViewArray[3] = (TextView) findViewById(R.id.TextView08);
         textViewArray[4] = (TextView) findViewById(R.id.TextView09);
+        // Open Shared Preferences
+        mPrefs = getSharedPreferences(LocationUtils.SHARED_PREFERENCES, Context.MODE_PRIVATE);
+
+        // Get an editor
+        mEditor = mPrefs.edit();
     }
 
+    /*
+    * Called when the Activity is no longer visible at all.
+    * Stop updates and disconnect.
+    */
+    @Override
+    public void onStop() {
+
+        // If the client is connected
+        if (mLocationClient.isConnected()) {
+            stopPeriodicUpdates();
+        }
+
+        // After disconnect() is called, the client is considered "dead".
+        mLocationClient.disconnect();
+
+        super.onStop();
+    }
+    /*
+     * Called when the Activity is going into the background.
+     * Parts of the UI may be visible, but the Activity is inactive.
+     */
+    @Override
+    public void onPause() {
+
+        // Save the current setting for updates
+        mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, mUpdatesRequested);
+        mEditor.commit();
+        super.onPause();
+    }
+    /*
+     * Called when the Activity is restarted, even before it becomes visible.
+     */
+    @Override
+    public void onStart() {
+
+        super.onStart();
+
+        /*
+         * Connect the client. Don't re-start any requests here;
+         * instead, wait for onResume()
+         */
+        mLocationClient.connect();
+
+    }
+    /*
+     * Called when the system detects that this Activity is now visible.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // If the app already has a setting for getting location updates, get it
+        if (mPrefs.contains(LocationUtils.KEY_UPDATES_REQUESTED)) {
+            mUpdatesRequested = mPrefs.getBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
+
+            // Otherwise, turn off location updates until requested
+        } else {
+            mEditor.putBoolean(LocationUtils.KEY_UPDATES_REQUESTED, false);
+            mEditor.commit();
+        }
+
+    }
     public static Handler returnHandler()
     {
         return hm;
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -200,7 +277,35 @@ public class MainActivity extends FragmentActivity implements
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+         /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
 
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+
+            } catch (IntentSender.SendIntentException e) {
+
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+
+            // If no resolution is available, display a dialog to the user with the error.
+            showErrorDialog(connectionResult.getErrorCode());
+        }
     }
 
     /**
@@ -224,6 +329,11 @@ public class MainActivity extends FragmentActivity implements
             }
             else {
                 fragment = new NearbyFragment();
+                mUpdatesRequested = true;
+
+                if (servicesConnected()) {
+                    startPeriodicUpdates();
+                }
             }
 
             //Bundle args = new Bundle();
@@ -452,5 +562,137 @@ public class MainActivity extends FragmentActivity implements
         }//if isIntentSafe
         return false;
     }
+
+
+
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+
+
+    }
+
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Choose what to do based on the request code
+        switch (requestCode) {
+
+            // If the request code matches the code sent in onConnectionFailed
+            case LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST :
+
+                switch (resultCode) {
+                    // If Google Play services resolved the problem
+                    case Activity.RESULT_OK:
+
+                        // Log the result
+                        Log.d(LocationUtils.APPTAG, getString(R.string.resolved));
+
+
+                        break;
+
+                    // If any other result was returned by Google Play services
+                    default:
+                        // Log the result
+                        Log.d(LocationUtils.APPTAG, getString(R.string.no_resolution));
+
+
+                        break;
+                }
+
+                // If any other request code was received
+            default:
+                // Report that this Activity received an unknown requestCode
+                Log.d(LocationUtils.APPTAG,
+                        getString(R.string.unknown_activity_request_code, requestCode));
+
+                break;
+        }
+    }
+
+    private boolean servicesConnected() {
+        // Check that Google Play services is available
+        int resultCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d(LocationUtils.APPTAG, getString(R.string.play_services_available));
+
+            // Continue
+            return true;
+            // Google Play services was not available for some reason
+        } else {
+            // Display an error dialog
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(dialog);
+                errorFragment.show(getFragmentManager(), LocationUtils.APPTAG);
+            }
+            return false;
+        }
+    }
+    private void showErrorDialog(int errorCode) {
+
+        // Get the error dialog from Google Play services
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+                errorCode,
+                this,
+                LocationUtils.CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+
+            // Create a new DialogFragment in which to show the error dialog
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+
+            // Show the error dialog in the DialogFragment
+            errorFragment.show(getFragmentManager(), LocationUtils.APPTAG);
+        }
+    }
+    /**
+     * In response to a request to start updates, send a request
+     * to Location Services
+     */
+    private void startPeriodicUpdates() {
+
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+
+    }
+
+    /**
+     * In response to a request to stop updates, send a request to
+     * Location Services
+     */
+    private void stopPeriodicUpdates() {
+        mLocationClient.removeLocationUpdates(this);
+
+    }
+
+
+
 
 }
